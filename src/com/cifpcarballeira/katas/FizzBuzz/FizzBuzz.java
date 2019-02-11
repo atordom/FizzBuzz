97422aa74ef9ea0970f96c93a1092c81c53ef1dd
alex <fulano@mengano.com>
2019-02-11 11:44:25 +0100

    Primeiro commit

---
+++ src/com/cifpcarballeira/katas/FizzBuzz/FizzBuzz.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cifpcarballeira.katas.FizzBuzz;
+
+/**
+ *
+ * @author alex
+ */
+public class FizzBuzz {
+    
+}
