9fe071f9a89fbcc655f90164fd7e2380288d2aef
alex <fulano@mengano.com>
2019-02-12 09:48:25 +0100

    2 devuelve 2

--- test/com/cifpcarballeira/katas/TestFizzBuzz/TestFizzBuzz.java
+++ test/com/cifpcarballeira/katas/TestFizzBuzz/TestFizzBuzz.java
@@ -29,4 +29,12 @@ public class TestFizzBuzz {
     assertEquals("1",result);
     
     }
+    
+    @Test
+    public void test2devuelve2() {
+    FizzBuzz fb = new FizzBuzz();
+    String result = fb.getFizzBuzz(2);
+    assertEquals("2",result);
+    
+    }
 }
